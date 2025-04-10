8eaadd230565ebbef8d860dbc4277712b351d717
ABHIRAM GATTAMNENI <abhiram050904@gmail.com>
2025-04-10 12:06:42 +0530

    change

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,47 @@
+const prisma = require("../configurations/db");
+const jwt = require("jsonwebtoken");
+require("dotenv").config(); // Load environment variables
+
+const login = async (req, res) => {
+    try {
+        const { name, email, provider, oauth_id, image } = req.body;
+
+        let findUser = await prisma.user.findUnique({
+            where: { email }
+        });
+
+        if (!findUser) {
+            findUser = await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                    provider,
+                    oauth_id,
+                    image
+                }
+            });
+        }
+
+        // Generate JWT token
+        const token = jwt.sign(
+            { userId: findUser.id, email: findUser.email }, // Payload
+            process.env.JWT_SECRET, // Secret key
+            { expiresIn: "7d" } // Token expiry (7 days)
+        );
+
+        // Set Authorization header
+        res.setHeader("Authorization", `Bearer ${token}`);
+
+        return res.status(200).json({ 
+            success: true, 
+            user: findUser, 
+            token 
+        });
+
+    } catch (err) {
+        console.error("Error in login:", err);
+        return res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+};
+
+module.exports = { login };
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const express=require('express')
 const cors=require('cors')
 const dotenv=require('dotenv')
-const sql=require('./db')
+const userroutes=require('./routes/userroutes')
 dotenv.config()
 
 const port=process.env.PORT
 const app=express()
 app.use(cors())
 app.use(express.json())
+app.use('/api',userroutes)
 app.use(express.urlencoded({extended:false}))
 app.listen(port,()=>{
     console.log(`app is runing on port:`,port )
